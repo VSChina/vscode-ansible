6cfa3c1b064257e27102add92e97ce74d5dd03b3
yungez <yungez@microsoft.com>
2017-11-09 15:09:30 +0800

    initial checkin

---
+++ extension.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const vscode = require('vscode');
+const utilities = require('./lib/utilities.js');
+
+
+function activate(context) {
+    console.log('Congratulations, your extension "vsc-extension-ansible" is now active!');
+    var outputChannel = vscode.window.createOutputChannel("VSCode extension for Ansible");
+
+    var playbook = vscode.window.activeTextEditor.document.fileName;
+
+    let runpb = vscode.commands.registerCommand('extension.ansible-playbook', function () {
+        vscode.window.showInputBox({ value: playbook, prompt: 'Please input playbook name', placeHolder: 'playbook', password: false })
+            .then((input) => {
+                if (input != undefined && input != '') {
+                    playbook = input;
+                }
+                utilities.runPlayBook(playbook, outputChannel);
+            })
+    });
+
+    context.subscriptions.push(runpb);
+}
+
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+
+exports.activate = activate;
+exports.deactivate = deactivate;
