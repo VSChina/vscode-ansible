e14dd4df113877ae9b78e2af7649add26cc70782
zikalino <zikalino@microsoft.com>
2017-11-17 15:13:28 +0800

    i think test is now fixed

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,22 +1,58 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
+/* global suite, test */
 
-// The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-import * as myExtension from '../extension';
+const extensionId = 'yungez.vsc-extension-ansible';
 
-// Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", () => {
 
-    // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    test("should be present", function () {
+        assert.ok(vscode.extensions.getExtension(extensionId));
     });
+
+    test("should be able to activate", function () {
+        this.timeout(5 * 1000);
+        const extension = vscode.extensions.getExtension(extensionId);
+        if (!extension.isActive) {
+            extension.activate().then(function () {
+                assert.ok('extension activated');
+            }, function () {
+                assert.fail(false, true, 'extension failed to activate!');
+            })
+        }
+    })
+
+    test("should be able to register ansible commands", function () {
+        const extension = vscode.extensions.getExtension(extensionId);
+        extension.activate().then(function () {
+            return vscode.commands.getCommands(true).then(function (commands) {
+                const COMMANDS = [
+                    'vsc-extension-ansible.ansible-playbook',
+                    'vsc-extension-ansible.ansible-commands'
+                ].sort();
+
+                var foundCmds = commands.filter(function (e) {
+                    return e.startsWith('vsc-extension-ansible');
+                }).sort();
+
+                assert.equal(foundCmds.length, COMMANDS.length, 'some commands are not registered properly');
+            }, function () {
+                assert.fail(false, true, 'failed to getCommands!');
+            })
+        });
+    })
+
+    test("should be able to run ansible command", function () {
+        const extension = vscode.extensions.getExtension(extensionId);
+        extension.activate().then(function () {
+            vscode.commands.executeCommand('vsc-extension-ansible.ansible-commands', 'ansible --version').then(
+                function (result) {
+                    assert.ok('cmd run done.' + result);
+                },
+                function () {
+                    assert.fail(false, true, 'failed to run ansible cmd!');
+                }
+            )
+        })
+    })
 });
