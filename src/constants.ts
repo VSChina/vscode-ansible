7ad35f7d888d2580be4ef97a45609e773f050d2b
yungez <yungez@microsoft.com>
2017-12-04 16:35:48 +0800

    fix hardcode timeout after terminal Init cmd (#29)
    * fix hardcode timeout after terminal Init cmd
    
    * fix issues found during linux testing

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
-    public static ExtensionId = 'vscode-ansible';
+    public static ExtensionId = 'yungez.vscode-ansible';
     public static LineSeperator = Array(50).join('=');
     public static AzureAccountExtensionId = 'ms-vscode.azure-account';
     public static DockerImageName = 'dockiot/ansible';
