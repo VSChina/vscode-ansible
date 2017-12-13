f9f0f27bf4d10bf4fc8862ee6e093b8a02c482d0
yungez <yungez@microsoft.com>
2017-12-13 15:40:23 +0800

    use official release docker image

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export class Constants {
     public static ExtensionId = 'yungez.vscode-ansible';
     public static LineSeperator = Array(50).join('=');
     public static AzureAccountExtensionId = 'ms-vscode.azure-account';
-    public static DockerImageName = 'dockiot/ansible';
+    public static DockerImageName = 'microsoft/ansible:ubuntu1604';
     public static AnsibleTerminalName = 'ansible';
     public static UserAgentName = 'VSCODEEXT_USER_AGENT';    
 }
