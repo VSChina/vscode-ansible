d44efcce0f938406b88e25e4af7eb02b22229aa4
yungez <yungez@microsoft.com>
2017-12-15 14:28:25 +0800

    remove useless comments

--- src/completionEngine.ts
+++ src/completionEngine.ts
@@ -14,12 +14,8 @@ export class CompletionEngine {
     }
 
     public getCompletionItems(prefix: string, line: string): Promise<CompletionItem[]> {
-        //let moduleRegexp = new RegExp(`^\\s*-?\\s+(action\\s*:\\s+|local_action\\s*:\\s+|)${prefix}$`);
-
         let result: CompletionItem[] = [];
-        // let match = line.match(moduleRegexp);
-        // if (match) {
-        //     if (!match[1]) {
+        
         if (/^with_/.test(prefix)) {
             Array.prototype.push.apply(result, getFuzzySuggestions(this.data.loopDirectives, prefix));
         }
