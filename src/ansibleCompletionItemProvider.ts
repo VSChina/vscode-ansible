f23cb35a4a4928b3234992d673de156d6139a4e8
Yunge Zhu <37337818+yungezz@users.noreply.github.com>
2018-04-19 02:20:21 -0700

    only provide autocompletion for property name (#110)
    * only provide autocompletion for property name
    
    * add comments

--- src/ansibleCompletionItemProvider.ts
+++ src/ansibleCompletionItemProvider.ts
@@ -22,6 +22,12 @@ export class AnsibleCompletionItemProvider implements vscode.CompletionItemProvi
         let prefix = range ? document.getText(range) : '';
         let lineText = document.lineAt(position.line).text;
 
+        // provide auto completion for property name only, not on value
+        var index = lineText.indexOf(':');
+        if (index != -1 && index < range.end.character) {
+            return;
+        }
+
         if (pattern_variable.exec(lineText)) {
             return this.completionEngine.getVariablesCompletionItem(document, prefix, lineText);
         } else {
