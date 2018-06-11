12ec90dd56a2527fdb254d9e47e888318a30e43a
yungez <yungez@microsoft.com>
2018-06-11 11:41:49 +0800

    add config for copy workspace

--- src/sshRunner.ts
+++ src/sshRunner.ts
@@ -61,13 +61,10 @@ export class SSHRunner extends TerminalBaseRunner {
         let targetPlaybook = target;
 
 
-        // ask for weather to sync workspace
-        const cancelItem: vscode.MessageItem = { title: "Cancel" };
-        const okItem: vscode.MessageItem = { title: "Ok" };
-        let response = await vscode.window.showWarningMessage('Copy Workspace to Remote host?', okItem, cancelItem);
+        // check configuration weather to sync workspace
+        let copyWorkspace = utilities.getCodeConfiguration<boolean>('ansible', 'copyWorkspace');
 
-        if (response && response === okItem) {
-
+        if (copyWorkspace) {
             src = utilities.getWorkspaceRoot(playbook) + '/';
             target = path.join('\./', path.basename(src)) + '/';
             targetPlaybook = ['\./' + path.basename(src), path.relative(src, playbook)].join(path.posix.sep).replace(/\\/g, '/');
@@ -77,10 +74,7 @@ export class SSHRunner extends TerminalBaseRunner {
             } catch (err) {
                 return;
             }
-        }
-
-        if (!response || response === cancelItem) {
-
+        } else {
             this._outputChannel.append('\nCopying ' + src + ' to ' + targetServer.host + '..');
             this._outputChannel.show();
 
