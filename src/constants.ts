c8ea0a66742caba086af0d5a6986f2f9f873cf44
Rafael Chicoli <rafael_chicoli@yahoo.com.br>
2018-09-24 22:22:02 +0200

    sort constant ascending

--- src/constants.ts
+++ src/constants.ts
@@ -1,20 +1,20 @@
 export class Constants {
-    public static ExtensionId = 'vscoss.vscode-ansible';
-    public static LineSeperator = Array(50).join('=');
-    public static AzureAccountExtensionId = 'ms-vscode.azure-account';
-    public static DockerImageName = 'microsoft/ansible:latest';
     public static AnsibleTerminalName = 'Ansible';
-    public static UserAgentName = 'VSCODEEXT_USER_AGENT';
+    public static AzureAccountExtensionId = 'ms-vscode.azure-account';
+    public static AzureManagementApiHost = 'management.azure.com';
+    public static AzureQuickStartTemplates = 'Azure/azure-quickstart-templates';
     public static Config_cloudShellConfirmed = 'cloudShellConfirmed';
     public static Config_credentialConfigured = 'credentialsConfigured';
     public static Config_credentialsFile = 'credentialsFile';
     public static Config_dockerImage = 'dockerImage';
     public static Config_terminalInitCommand = 'terminalInitCommand';
+    public static DockerImageName = 'microsoft/ansible:latest';
+    public static ExtensionId = 'vscoss.vscode-ansible';
     public static GitHubApiHost = 'api.github.com';
     public static GitHubRawContentHost = 'raw.githubusercontent.com';
-    public static AzureQuickStartTemplates = 'Azure/azure-quickstart-templates';
-    public static AzureManagementApiHost = 'management.azure.com';
+    public static LineSeperator = Array(50).join('=');
     public static NotShowThisAgain = "NotShowThisAgain";
+    public static UserAgentName = 'VSCODEEXT_USER_AGENT';
 }
 
 export enum CloudShellErrors {
